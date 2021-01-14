88513dd3f16f18558e9d5e24a7e5c2898df45675
Justin Seby <74730607+justinseby@users.noreply.github.com>
2021-01-14 14:21:35 +0530

    Add files via upload

---
+++ Backend Microservices/zuulserver/src/main/java/com/example/zuulserver/ZuulserverApplication.java
@@ -0,0 +1,33 @@
+package com.example.zuulserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class ZuulserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulserverApplication.class, args);
+	}
+	@Bean
+    public PreFilter preFilter() {
+        return new PreFilter();
+    }
+    @Bean
+    public PostFilter postFilter() {
+        return new PostFilter();
+    }
+    @Bean
+    public ErrorFilter errorFilter() {
+        return new ErrorFilter();
+    }
+    @Bean
+    public RouteFilter routeFilter() {
+        return new RouteFilter();
+    }
+}
