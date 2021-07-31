98458479a31bd3fdbb0525bb3a3260d02cfdec79
Justin Seby <74730607+justinseby@users.noreply.github.com>
2021-07-31 13:16:21 +0530

    Update FlightEntity.java

--- Backend Microservices/booking/src/main/java/com/example/booking/entity/FlightEntity.java
+++ Backend Microservices/booking/src/main/java/com/example/booking/entity/FlightEntity.java
@@ -10,6 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+
 @Entity
 @Table(name = "flight", schema="public")
 public class FlightEntity implements Serializable{
