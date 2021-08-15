b6f25505af6b8571ddcb9d78465173abd0b3331f
Justin Seby <74730607+justinseby@users.noreply.github.com>
2021-08-15 22:19:11 +0530

    Update FlightEntity.java

--- Backend Microservices/Flight/src/main/java/com/example/Flight/entity/FlightEntity.java
+++ Backend Microservices/Flight/src/main/java/com/example/Flight/entity/FlightEntity.java
@@ -2,7 +2,6 @@ package com.example.Flight.entity;
 
 import java.io.Serializable;
 import java.sql.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
